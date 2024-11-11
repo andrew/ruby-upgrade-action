d63013e54042d3065472837d461241cfbcaf4768
Andrew Nesbitt <andrewnez@gmail.com>
2024-11-11 08:24:54 +0000

    Remove unused files

--- scripts/fetch_latest_ruby.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-LATEST_VERSION=$(curl -s https://www.ruby-lang.org/en/downloads/ | grep -oP 'Ruby \K[0-9]+\.[0-9]+\.[0-9]+' | head -1)
-echo "Latest Ruby version detected: $LATEST_VERSION"
-echo "LATEST_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
